a90e1aeb485eeb8d2f67bf607cdbd78994ab7450
Joao <joao_quennehen@outlook.com>
2025-08-11 20:45:49 -0300

    feat: json update

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Cadastro from "./src/screens/Cadastro";
+import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 
 const Stack = createNativeStackNavigator();
@@ -19,6 +20,7 @@ export default function App() {
           component={Cadastro}
           options={{ headerShown: false }}
         />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
