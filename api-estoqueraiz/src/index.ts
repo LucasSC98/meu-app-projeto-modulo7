e2d0d2465cebb9bef1f08d1624b5b5a604adda53
LucasSC98 <lucas.custodio@grupointegrado.br>
2025-08-18 23:43:27 -0300

    refactor: alternando nomes para seguir padrão

--- api-estoqueraiz/src/index.ts
+++ api-estoqueraiz/src/index.ts
@@ -2,31 +2,56 @@ import express from "express";
 import sequelize from "./config/database";
 import rotaLogin from "./routes/rotaLogin";
 import rotaUsuarios from "./routes/rotaUsuarios";
+import rotaCategoria from "./routes/rotaCategoria";
+import rotaUnidade from "./routes/rotaUnidade";
+import rotaProdutos from "./routes/rotaProdutos";
+import "./models/UnidadesModel";
+import "./models/ProdutosModel";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Estoque Raiz",
+      version: "1.0.0",
+      description: "Documentação da API com Swagger",
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.use(express.json());
 app.use("/usuarios", rotaUsuarios);
 app.use(rotaLogin);
-
+app.use("/categorias", rotaCategoria);
+app.use("/unidades", rotaUnidade);
+app.use("/produtos", rotaProdutos);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.get("/", (req, res) => {
   res.send("Olá, mundo!");
 });
 
 async function iniciarAplicacao() {
-  await sequelize.authenticate();
-  app.listen(PORT, () => {
-    console.log(`O servidor está rodando na porta ${PORT}`);
-  });
+  try {
+    await sequelize.authenticate();
+    console.log("Conexão com banco de dados estabelecida com sucesso!");
 
-  sequelize
-    .sync({ alter: true })
-    .then(() => {
-      console.log("database foi sincronizado com sucesso");
-    })
-    .catch((error) => {
-      console.log("deu zica no bagulho", error);
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
+
+    await sequelize.sync({ alter: true });
+    console.log("Banco de dados sincronizado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao iniciar aplicação:", error);
+  }
 }
 
 iniciarAplicacao();
