f98c606980cba2ed2a9ca52a5941bcb3adb7ee00
LucasSC98 <lucas.custodio@grupointegrado.br>
2025-08-28 13:48:49 -0300

    chore: alterando base url

--- estoqueraiz-app/src/services/api.tsx
+++ estoqueraiz-app/src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.7:3000",
+  baseURL: "http://192.168.1.19:3000",
 });
 
 api.interceptors.request.use(
@@ -14,6 +14,17 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+// Interceptor de resposta para tratamento de erros
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.code === "NETWORK_ERROR") {
+      console.error("Erro de conexão - Verifique se a API está rodando");
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
